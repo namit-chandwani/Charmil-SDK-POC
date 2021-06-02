f5b4b3a2928e1140a2e116ab08da63f38aad683d
Namit Chandwani <namitchandwani99@gmail.com>
2021-06-02 07:27:31 +0530

    refactor: move plugin loader code into single file

--- pkg/core/pluginloader.go
+++ pkg/core/pluginloader.go
@@ -14,6 +14,35 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+type PluginConfig struct {
+	Commands []CommandConfig `yaml:"commands"`
+}
+
+// CommandConfig
+type CommandConfig struct {
+	Name             string        `yaml:"name"`
+	MapsTo           ArgsMapConfig `yaml:"mapsTo"`
+	Flags            []FlagConfig  `yaml:"flags"`
+	ShortDescription string        `yaml:"shortDescription"`
+	Examples         string        `yaml:"usage"`
+}
+
+type ArgsMapConfig struct {
+	Name       string
+	Subcommand string
+	Args       []string
+}
+
+// FlagConfig
+type FlagConfig struct {
+	Type         string `yaml:"type"`
+	DefaultValue string `yaml:"defaultValue"`
+	Name         string `yaml:"name"`
+	MapsTo       string `yaml:"mapsTo"`
+	Description  string `yaml:"description"`
+	Alias        string `yaml:"alias"`
+}
+
 func NewCommand(cmd *cobra.Command) error {
 	cwd, err := os.Getwd()
 	if err != nil {
